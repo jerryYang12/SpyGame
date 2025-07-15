6fe1328f57c32cd4d46134a4ec64a686a9ee3834
HP-JY\JerryYang <jerry.yang@unisys.com>
2025-07-15 20:35:44 +0800

    v6

--- config.js
+++ config.js
@@ -1,97 +1,39 @@
 window.sets = [
-  [ { text: "貓 Cat", img: "" }, { text: "狗 Dog", img: "" } ],
-  [ { text: "太陽 Sun", img: "" }, { text: "月亮 Moon", img: "" } ],
-  [ { text: "水 Water", img: "" }, { text: "火 Fire", img: "" } ],
-  [ { text: "天空 Sky", img: "" }, { text: "大地 Earth", img: "" } ],
-  [ { text: "魚 Fish", img: "" }, { text: "鳥 Bird", img: "" } ],
-  [ { text: "車 Car", img: "" }, { text: "飛機 Plane", img: "" } ],
-  [ { text: "花 Flower", img: "" }, { text: "草 Grass", img: "" } ],
-  [ { text: "星星 Star", img: "" }, { text: "雲 Cloud", img: "" } ],
-  [ { text: "山 Mountain", img: "" }, { text: "河 River", img: "" } ],
-  [ { text: "電腦 Computer", img: "" }, { text: "手機 Phone", img: "" } ],
-  [ { text: "書 Book", img: "" }, { text: "筆 Pen", img: "" } ],
-  [ { text: "鉛筆 Pencil", img: "" }, { text: "橡皮 Eraser", img: "" } ],
-  [ { text: "餅乾 Cookie", img: "" }, { text: "糖果 Candy", img: "" } ],
-  [ { text: "蘋果 Apple", img: "" }, { text: "香蕉 Banana", img: "" } ],
-  [ { text: "牛奶 Milk", img: "" }, { text: "咖啡 Coffee", img: "" } ],
-  [ { text: "桌子 Table", img: "" }, { text: "椅子 Chair", img: "" } ],
-  [ { text: "鞋子 Shoes", img: "" }, { text: "衣服 Clothes", img: "" } ],
-  [ { text: "鉛筆盒 Pencil Case", img: "" }, { text: "書包 Backpack", img: "" } ],
-  [ { text: "雨傘 Umbrella", img: "" }, { text: "帽子 Hat", img: "" } ],
-  [ { text: "電視 TV", img: "" }, { text: "收音機 Radio", img: "" } ],
-  [ { text: "手錶 Watch", img: "" }, { text: "項鍊 Necklace", img: "" } ],
-  [ { text: "電話 Telephone", img: "" }, { text: "郵件 Mail", img: "" } ],
-  [ { text: "電燈 Light", img: "" }, { text: "風扇 Fan", img: "" } ],
-  [ { text: "門 Door", img: "" }, { text: "窗 Window", img: "" } ],
-  [ { text: "球 Ball", img: "" }, { text: "蝴蝶 Butterfly", img: "" } ],
-  [ { text: "老師 Teacher", img: "" }, { text: "學生 Student", img: "" } ],
-  [ { text: "車站 Station", img: "" }, { text: "機場 Airport", img: "" } ],
-  [ { text: "火車 Train", img: "" }, { text: "公車 Bus", img: "" } ],
-  [ { text: "海洋 Ocean", img: "" }, { text: "沙灘 Beach", img: "" } ],
-  [ { text: "牛 Cow", img: "" }, { text: "羊 Sheep", img: "" } ],
-  [ { text: "猴子 Monkey", img: "" }, { text: "熊 Bear", img: "" } ],
-  [ { text: "蛇 Snake", img: "" }, { text: "龜 Turtle", img: "" } ],
-  [ { text: "電影 Movie", img: "" }, { text: "音樂 Music", img: "" } ],
-  [ { text: "城市 City", img: "" }, { text: "鄉村 Village", img: "" } ],
-  [ { text: "雪 Snow", img: "" }, { text: "冰 Ice", img: "" } ],
-  [ { text: "鳳梨 Pineapple", img: "" }, { text: "西瓜 Watermelon", img: "" } ],
-  [ { text: "麵包 Bread", img: "" }, { text: "蛋糕 Cake", img: "" } ],
-  [ { text: "蜜蜂 Bee", img: "" }, { text: "螞蟻 Ant", img: "" } ],
-  [ { text: "燈塔 Lighthouse", img: "" }, { text: "燈籠 Lantern", img: "" } ],
-  [ { text: "螢火蟲 Firefly", img: "" }, { text: "蝙蝠 Bat", img: "" } ],
-  [ { text: "飛鳥 Flying Bird", img: "" }, { text: "跑馬 Running Horse", img: "" } ],
-  [ { text: "城堡 Castle", img: "" }, { text: "橋 Bridge", img: "" } ],
-  [ { text: "火山 Volcano", img: "" }, { text: "沙漠 Desert", img: "" } ],
-  [ { text: "雨 Rain", img: "" }, { text: "雷 Thunder", img: "" } ],
-  [ { text: "雲朵 Cloud", img: "" }, { text: "風 Wind", img: "" } ],
-  [ { text: "花園 Garden", img: "" }, { text: "森林 Forest", img: "" } ],
-  [ { text: "醫院 Hospital", img: "" }, { text: "藥局 Pharmacy", img: "" } ],
-  [ { text: "餐廳 Restaurant", img: "" }, { text: "咖啡館 Cafe", img: "" } ],
-  [ { text: "河流 Stream", img: "" }, { text: "湖泊 Lake", img: "" } ],
-  [ { text: "演員 Actor", img: "" }, { text: "歌手 Singer", img: "" } ],
-  [ { text: "眼睛 Eye", img: "" }, { text: "耳朵 Ear", img: "" } ],
-  [ { text: "鼻子 Nose", img: "" }, { text: "嘴巴 Mouth", img: "" } ],
-  [ { text: "手 Hand", img: "" }, { text: "腳 Foot", img: "" } ],
-  [ { text: "心臟 Heart", img: "" }, { text: "腦 Brain", img: "" } ],
-  [ { text: "黑色 Black", img: "" }, { text: "白色 White", img: "" } ],
-  [ { text: "橘子 Orange", img: "" }, { text: "葡萄 Grape", img: "" } ],
-  [ { text: "草莓 Strawberry", img: "" }, { text: "桃子 Peach", img: "" } ],
-  [ { text: "蘿蔔 Carrot", img: "" }, { text: "馬鈴薯 Potato", img: "" } ],
-  [ { text: "番茄 Tomato", img: "" }, { text: "洋蔥 Onion", img: "" } ],
-  [ { text: "汽車 Car", img: "" }, { text: "腳踏車 Bicycle", img: "" } ],
-  [ { text: "飛機場 Airport", img: "" }, { text: "港口 Port", img: "" } ],
-  [ { text: "銀行 Bank", img: "" }, { text: "郵局 Post Office", img: "" } ],
-  [ { text: "廚房 Kitchen", img: "" }, { text: "浴室 Bathroom", img: "" } ],
-  [ { text: "書架 Bookshelf", img: "" }, { text: "桌子 Table", img: "" } ],
-  [ { text: "沙發 Sofa", img: "" }, { text: "床 Bed", img: "" } ],
-  [ { text: "手電筒 Flashlight", img: "" }, { text: "蠟燭 Candle", img: "" } ],
-  [ { text: "手套 Gloves", img: "" }, { text: "帽子 Hat", img: "" } ],
-  [ { text: "皮包 Wallet", img: "" }, { text: "背包 Backpack", img: "" } ],
+
   [ { text: "刀 Fork", img: "" }, { text: "叉 Spoon", img: "" } ],
-  [ { text: "筷子 Chopsticks", img: "" }, { text: "湯匙 Spoon", img: "" } ],[
+  [ { text: "筷子 Chopsticks", img: "" }, { text: "湯匙 Spoon", img: "" } ],
+  [
+	  { text": 棉花糖 Marshmallow", img: "https://cityfm.tw/Public/MyMedia/CityNews/20181226-1opt.jpg" },
+	  { text": 巧克力 Chocolate", img": "https://upload.wikimedia.org/wikipedia/commons/7/70/Chocolate_%28blue_background%29.jpg" }
+  ],
+  [
+	{ text: "瑪利歐 Mario", img: "https://images.ctfassets.net/o5v89n4kg6h4/2QECPUVaKbEvGv8SeIlDWo/9b995b0ffa3991511c603cf0983e8f85/2-1.jpg?w=738&fm=webp"},
+	{ text: "路易吉 Luigi", img: "https://static.wikia.nocookie.net/mario/images/2/22/Luigi_MKGP2.jpg/revision/latest/scale-to-width-down/250?cb=20100717170346"}
+  ],
   [ 
     { text: "屁屁偵探 Butt Detective", img: "https://www.avex.com.tw/Butt-Detective/images/kv-peo.svg" }, 
     { text: "柯南 Conan", img: "https://s.yimg.com/ny/api/res/1.2/8kTPSQoscFrR0L0Tni5dIA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTcwNTtoPTQ0Mw--/https://s.yimg.com/os/creatr-uploaded-images/2021-07/ad267e70-e14b-11eb-bde7-a95c48555207" } 
   ],
   [ 
     { text: "皮卡丘 Pikachu", img: "https://upload.wikimedia.org/wikipedia/zh/7/77/Pikachu.png" }, 
-    { text: "卡比獸 Snorlax", img: "https://upload.wikimedia.org/wikipedia/zh/0/0b/Snorlax.png" }, 
+    { text: "卡比獸 Snorlax", img: "https://upload.wikimedia.org/wikipedia/zh/0/0b/Snorlax.png" } 
   ],
   [ 
     { text: "超夢 Mewtwo", img: "https://upload.wikimedia.org/wikipedia/zh/d/d3/Mewtwo.png" }, 
-    { text: "謎擬Q Mimikyu", img: "https://upload.wikimedia.org/wikipedia/zh/2/23/Pok%C3%A9mon_Mimikyu_art.png" }, 
+    { text: "謎擬Q Mimikyu", img: "https://upload.wikimedia.org/wikipedia/zh/2/23/Pok%C3%A9mon_Mimikyu_art.png" } 
   ],
   [ 
     { text: "小傑 Gon", img: "https://bkimg.cdn.bcebos.com/pic/cc11728b4710b9128bb4b4f9cdfdfc03924522bb?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536" }, 
-    { text: "奇犽 Killua", img: "https://bkimg.cdn.bcebos.com/pic/77094b36acaf2eddee6b58858b1001e9380193c7?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536" }, 
+    { text: "奇犽 Killua", img: "https://bkimg.cdn.bcebos.com/pic/77094b36acaf2eddee6b58858b1001e9380193c7?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536" } 
   ],
   [ 
     { text: "雷歐力 Leorio", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7nmun5Q7puRZV_5wi1b_z_WBiS3BIPUs1-Q&s" }, 
-    { text: "庫拉皮卡 Kurapika", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREKdUEz1Ba7CFSDsz2cRBaIxdDC3mHDN6Yy3fNDlUnu1FmJJIPsLdKibZ7gmUesN9Iv6c&usqp=CAU" }, 
-  ]
-]
-
-  [ { text: "蟑螂 Cockroach", img: "" }, { text: "蜘蛛 Spider", img: "" } ],
+    { text: "庫拉皮卡 Kurapika", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREKdUEz1Ba7CFSDsz2cRBaIxdDC3mHDN6Yy3fNDlUnu1FmJJIPsLdKibZ7gmUesN9Iv6c&usqp=CAU" } 
+  ],
+  [ 
+	{ text: "蟑螂 Cockroach", img: "https://stickershop.line-scdn.net/stickershop/v1/product/24129127/LINEStorePC/main.png?v=2" }, 
+	{ text: "蜘蛛 Spider", img: "https://img.freepik.com/free-photo/red-black-tarantula_1308-41489.jpg?semt=ais_hybrid&w=740" } 
+  ],
   [ { text: "聖誕節 Christmas", img: "" }, { text: "過年 Lunar New Year", img: "" } ],
   [ { text: "樂高 LEGO", img: "" }, { text: "撲克牌 Poker Cards", img: "" } ]
 ];
